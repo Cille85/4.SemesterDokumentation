c377294fe4d613de601d55c39e2a364c71eb8091
Cæcilie Klinkwort <Cklinkwort@gmail.com>
2024-08-14 09:28:41 +0200

    Add project files.

---
+++ VitaTest/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace VitaTest
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
